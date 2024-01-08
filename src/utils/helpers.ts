c62cacbedf231817f1e38baeb88e716a49c6d0c2
Shelly Paul <44363510+shellypaul93@users.noreply.github.com>
2024-01-08 16:34:18 -0500

    Phase 2: Added missed file to be staged

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,21 @@
 const isNullOrEmpty = (value: any) =>
   value === null ||
   value === undefined ||
-  value === '' ||
-  Object.keys(value).length === 0;
+  value === "" ||
+  Object.keys(value).length === 0 ||
+  (Array.isArray(value) && value.length === 0);
+
 const getCleanUrl = (path: string): string =>
-  decodeURIComponent(path).replace(/%20/g, ' ');
-export { isNullOrEmpty, getCleanUrl };
+  decodeURIComponent(path).replace(/%20/g, " ");
+
+function generateCodeVerifier(length: number): string {
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+export { isNullOrEmpty, getCleanUrl, generateCodeVerifier };
